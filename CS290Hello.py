027e8314705636eeee41d9accbe69c1717037945
Eric <eferrer23@gmail.com>
2015-09-28 00:32:17 -0700

    Adding first file
    First of many pushes this quarter

---
+++ CS290Hello.py
@@ -0,0 +1,14 @@
+#############################################
+## Program Filename: Video.cpp
+#
+# Author: Eric Ferrer
+#
+#Date: 7/6/15
+#
+# Description: Header file for Items
+#############################################
+
+
+print "Hello, CS 290 Class!"
+
+print "Just printing more stuff because I thought it was strange to have more comments than actual code."
