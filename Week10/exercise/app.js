395f03ec35df3506e247f9be2015f9d4229a7b4d
Eric <eferrer23@gmail.com>
2015-12-05 02:22:22 -0800

    ...

--- Week10/exercise/app.js
+++ Week10/exercise/app.js
@@ -3,13 +3,17 @@ var mysql = require('./dbcon.js');
 
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main'
+});
 
 var request = require('request');
 var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.engine('handlebars', handlebars.engine);
 
@@ -17,74 +21,83 @@ app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 
-app.get('/', function(req, res){   
+app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/',function(req,res,next){
+app.get('/', function (req, res, next) {
     var context = {};
-mysql.pool.query('SELECT * FROM workouts', function(err, rows,   fields){
-    if(err){
-        next(err);
-        return;
-            }
-    context.results = JSON.stringify(rows);
-    res.render('home', context);
+    mysql.pool.query('SELECT * FROM workouts', function (err, rows, fields) {
+        if (err) {
+            next(err);
+            return;
+        }
+        context.results = JSON.stringify(rows);
+        res.render('home', context);
     });
 });
 
-app.get('/reset-table',function(req,res,next){
-  var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS workouts+", function(err){ 
-    var createString = "CREATE TABLE workouts("+
-    "id INT PRIMARY KEY AUTO_INCREMENT,"+
-    "name VARCHAR(255) NOT NULL,"+
-    "reps INT,"+
-    "weight INT,"+
-    "date DATE,"+
-    "lbs BOOLEAN)";
-    mysql.pool.query(createString, function(err){
-      context.results = "Table reset";
-      res.render('home',context);
-    })
-  });
-});
-
- 
-
-
-app.post('/insert',function(req,res){
+app.get('/reset-table', function (req, res, next) {
     var context = {};
-    if(req.body['Exercise'])
-        {
-            console.log("HOO");
-            console.log(req.body);
-            mysql.pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `data`, `lbs`) VALUES (?, ?, ?, ?, ?)",
-   [req,body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
-        if(err){
-           next(err);
-            return;
-           }
-    context.results = "Inserted id " + result.insertId;        
-    res.render('home',context);
+    mysql.pool.query("DROP TABLE IF EXISTS workouts+", function (err) {
+        var createString = "CREATE TABLE workouts(" +
+            "id INT PRIMARY KEY AUTO_INCREMENT," +
+            "name VARCHAR(255) NOT NULL," +
+            "reps INT," +
+            "weight INT," +
+            "date DATE," +
+            "lbs BOOLEAN)";
+        mysql.pool.query(createString, function (err) {
+            context.results = "Table reset";
+            res.render('home', context);
+        })
+    });
 });
-        }
+
+
+
+
+app.post('/', function (req, res) {
+   // var context = {};
+    if (req.body['Exercise']) {
+        console.log("HOO");
+        console.log(req.body);
+        mysql.pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `data`, `lbs`) VALUES (?, ?, ?, ?, ?)", [req, body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function (err, result) {
+            if (err) {
+                next(err);
+                return;
+            }
+            var newId = result.insertid;
+
+            mysql.pool.query('SELECT * FROM workout WHERE id=?', [newId], function (err, rows, fields) {
+
+                if (err) {
+                    next(err);
+                    return;
+                }
+                
+               // context.results = "Inserted id " + result.insertId;
+                var data = rows;
+                data = JSON.stringify(data);
+                res.send(data);
+            });
         });
+    }
+})
 
 
 
-    
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
 });
 
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
--- Week10/exercise/public/js/yikes.js
+++ Week10/exercise/public/js/yikes.js
@@ -16,13 +16,40 @@ function magicHappens(){
         payload.date = document.getElementById('date').value;
         payload.lbs= document.getElementById('lbs').value;
         
-        req.open('POST', 'http://52.89.169.73:3000/insert', true)
+        req.open('POST', '/', true)
         req.setRequestHeader('Content-Type', 'application/json');
         req.addEventListener('load', function () {
             if (req.status >= 200 && req.status < 400) {
                 var response = JSON.parse(req.responseText);
                 
                 //alert(req.response); //debug
+                var mainTable = document.getElementById('mainTable');
+				var newRow = document.createElement('tr');
+				newRow.setAttribute('id', 'row' + response.id);
+				mainTable.appendChild(newRow);
+
+				for (var i = 0; i < 6; i++){
+					var newCell = document.createElement('td');
+					newRow.appendChild(newCell);
+				}
+
+				newRow.children[0].textContent = response.name;
+				newRow.children[1].textContent = response.reps;
+				newRow.children[2].textContent = response.weight;
+				newRow.children[3].textContent = response.date;
+				newRow.children[4].textContent = response.lbs;
+
+				var editDelete = document.createElement('form');
+				newRow.children[5].appendChild(editDelete);
+				editDelete.setAttribute('method', 'post');
+
+				for (var i = 0; i < 3; i++){
+					var newInput = document.createElement('input');
+					editDelete.appendChild(newInput);
+				}
+                
+                
+                
         
     }else{
         console.log("ERROR IN NETWORK REQUEST " + req.statusText);
