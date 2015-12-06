2f842e977c146cda00a346aeb61a6ff323926d35
Eric <eferrer23@gmail.com>
2015-12-06 12:29:52 -0800

    yikes

--- Week10/exercise/app.js
+++ Week10/exercise/app.js
@@ -10,9 +10,9 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use('/static', express.static('public'));
+//app.use('/static', express.static('public'));
 //app.use(express.static('public'));
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 
