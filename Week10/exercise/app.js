e81b209c4b363c0d2e8129925be0c3042faed94a
Eric <eferrer23@gmail.com>
2015-12-06 03:58:19 -0800

    jjj

--- Week10/exercise/app.js
+++ Week10/exercise/app.js
@@ -36,7 +36,7 @@ app.get('/reset-table', function (req, res, next) {
         mysql.pool.query(createString, function (err) {
            // context.results = "Table reset";
             //res.render('home', context);
-             res.sendFile('index.html');
+             res.sendFile('htmlform.html');
         })
     });
 });
