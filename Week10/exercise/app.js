6e60d0d5f559a3132f29d7ed6170dbd7adacb9e9
Eric <eferrer23@gmail.com>
2015-12-06 02:19:07 -0800

    sad

--- Week10/exercise/app.js
+++ Week10/exercise/app.js
@@ -87,7 +87,7 @@ app.post('/', function (req, res, next) {
 */
 
 
-app.get('/insert', function (req, res, next) {
+app.get('/', function (req, res, next) {
     var context = {};
     mysql.pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function (err, result) {
         if (err) {
