d1c12fc53ea27a1e76edffeb444296a3d5761be2
Eric <eferrer23@gmail.com>
2015-12-07 03:01:22 -0800

    yikes

--- Week10/exercise/app.js
+++ Week10/exercise/app.js
@@ -127,7 +127,7 @@ app.post('/', function (req, res, next) {
                                 res.send(JSON.stringify(rows));
                                 });
                         });
-};
+}
 
 
                     app.use(function (req, res) {
@@ -143,4 +143,4 @@ app.post('/', function (req, res, next) {
 
                     app.listen(app.get('port'), function () {
                         console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-                    })
+                    });
