b039f16c07f601df800f860d525848a4166d57b6
Eric <eferrer23@gmail.com>
2015-12-06 22:32:37 -0800

    34
    1

--- Week10/exercise/public/script.js
+++ Week10/exercise/public/script.js
@@ -109,15 +109,16 @@ function addRow(){
     ('exercise').addEventListener('click', function(event){
 		var req = new XMLHttpRequest();
 		var payload = {};
-
+        console.log("4");
 		payload.name = document.getElementById('name').value;
 		payload.reps = document.getElementById('reps').value;
 		payload.weight = document.getElementById('weight').value;
 		payload.date = document.getElementById('date').value;
 		payload.units = document.getElementById('lbs').value;
 		payload.workout = true;
-
-		req.open('POST', '/insert', true);
+            
+		req.open('POST', '/', true);
+        alert("3645");
 		req.setRequestHeader('Content-Type', 'application/json');
 		req.addEventListener('load',function(){
 			if(req.status >= 200 && req.status < 400){
